e66eba193e667715270ca889ff9733cd1ac628eb
KMagdi <46417302+kmagdi@users.noreply.github.com>
2023-04-14 21:07:57 +0200

    readme

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Testing db4free with NodeJS and ReactJS restFullApp
 
-- hosting MySQL database on db4free
+- hosting MySQL database on ~~db4free~~ HEROKU-JawDB
 - hosting server side on Render
 - hosting client side on Vercel
--- src/components/getData.jsx
+++ src/components/getData.jsx
@@ -10,19 +10,19 @@ export const getTodos = async () => {
 export const addItem = async (formdata) => {
   console.log("formdata:",formdata);
   const response = await axios.post(url+"/add",formdata);
-  return await response;
+  return  response;
 };
 
 
 export const delItem=async (id) => {
   const response = await axios.delete(url+"/del/"+id);
-  return await response;
+  return  response;
 }
 export const doneItem=async (id) => {
   const response = await axios.put(url+"/done/"+id);
-  return await response;
+  return  response;
 }
 export const delItems=async () => {
   const response = await axios.delete(url+"/delAll");
-  return await response;
+  return  response;
 }
